4cfe90cbe3c3a9ece88e23322abfd835bfe7bf56
gandhi <gandhi@gandhi-PC>
2016-08-24 06:37:20 +0530

    in constants.java file i modified username

--- deepaksirFramework/src/com/acttime/genericlib/Constants.java
+++ deepaksirFramework/src/com/acttime/genericlib/Constants.java
@@ -2,7 +2,10 @@ package com.acttime.genericlib;
 
 public interface Constants {
 
-	String browser= "firefox";
+	//global variables 
+	
+	
+	String browser= "firefox";;
 	
 	String url = "http://localhost/login.do";
 	String userName = "admin";
